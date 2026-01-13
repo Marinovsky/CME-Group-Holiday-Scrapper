c6fd73461e18005bf0a83bd58cc6652e976466f2
Ricardo Andrés Marino Rojas <47573394+Marinovsky@users.noreply.github.com>
2026-01-13 11:50:25 -0500

    Add descriptions and improve reasoning

--- scrapper.py
+++ scrapper.py
@@ -1,46 +1,40 @@
 from openai import OpenAI
-from pydantic import BaseModel, RootModel
-from typing import Dict, List
+from pydantic import BaseModel, Field
+from typing import List
 
 client = OpenAI()
 
 class DateWithTime(BaseModel):
-    date: str
-    hour: str
+    date: str = Field(description="Dates must be M/D/YYYY (no leading zeros)")
+    hour: str = Field(description="Times must be HH:MM:SS (24h)")
 
 class HolidayCalendarEntry(BaseModel):
-    holidays: List[str]
-    bankHolidays: List[str]
-    earlyCloses: List[DateWithTime]
-    reopens: List[DateWithTime]
+    holidays: List[str] = Field(description="Full holidays, this is, the market does not open again before closed on that day")
+    bankHolidays: List[str] = Field(description="Bank Holidays, this is, market reopens again before closed on that day")
+    earlyCloses: List[DateWithTime] = Field(description="Early market closes, this is, the market closes before the regular hour")
+    reopens: List[DateWithTime] = Field(description="Reopens, this is, the market opens again before closed on that holiday, usually at night and for next day trading")
 
 
 class HolidayCalendar(BaseModel):
-    grains: HolidayCalendarEntry
+    grains: HolidayCalendarEntry = Field(description="CME Group asset class 'Grains'")
 
 prompt2 = """
 You are a data extraction agent.
 
 TASK:
 Search the official CME Group website for the CME Group Holiday Calendar for the year 2026 and
-extract ALL available 2026 information related to:
-- Full holidays (this is, the market does not open again before closed on that day)
-- Early market closes
-- Reopens (this is, the market opens again before closed on that holiday, usually at night and for next day trading)
-- Bank holidays (this is, market re opens again before closed)
-- Asset classes: Grains
+extract ALL available 2026 information related to holiday schedule for CME group asset class
+'Grains'
 
 RULES:
 - Use only CME Group official domains.
-- Dates must be M/D/YYYY (no leading zeros).
-- Times must be HH:MM:SS (24h)
 - If a value is not available, omit it.
 - Do NOT invent missing data.
-- Return JSON only.
 """
 
 second_response = client.responses.parse(
     model = "gpt-5.2",
+    reasoning = {"effort": "medium"},
     tools=[{"type": "web_search"}],
     input = [{"role": "user", "content": prompt2}],
     text_format=HolidayCalendar
